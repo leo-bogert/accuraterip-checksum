5fc7ac7db857f9ac1ed6ac5163fda8154f4c25bd
leo-bogert <github@leo.bogert.de>
2012-10-28 16:02:02 +0100

    Version 1.1

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
 accuraterip-checksum
 ====================
 
-A C99 commandline program to compute the AccurateRip checksum of singletrack WAV files. Implemented according to http://www.hydrogenaudio.org/forums/index.php?showtopic=97603
+# Description:
+	A C99 commandline program to compute the AccurateRip checksum of singletrack WAV files.
+	Implemented according to http://www.hydrogenaudio.org/forums/index.php?showtopic=97603
+
+# Syntax:
+	accuraterip-checksum [--version1 / --version2 (default)] filename track_number total_tracks
+	
+# Output:
+	By default, the V2 (version 2) checksum will be printed.
+	You can also obtain the V1 checksum with the --version1 parameter.
+
+# Compiling:
+	libsndfile is used for reading the WAV files.
+	Therefore, on Ubuntu 12.04, make sure you have the following packages installed:
+		libsndfile1 (should be installed by default)
+		libsndfile1-dev
+		
+	The configuration files of an Eclipse project are included.
+	You can use "EGit" (Eclipse git) to import the whole repository.
+	It should as well ask you to import the project configuration then.
+
+# Author:
+	Leo Bogert (http://leo.bogert.de)
+
+# Version:
+	1.1
+
+# Donations:
+	bitcoin:14kPd2QWsri3y2irVFX6wC33vv7FqTaEBh
+	
+# License:
+	GPLv3
---
+++ accuraterip-checksum.c
@@ -0,0 +1,222 @@
+/*
+ ============================================================================
+ Name        : accuraterip-checksum.c
+ Author      : Leo Bogert (http://leo.bogert.de)
+ Version     : 1.1
+ Copyright   : GPL
+ Description : A C99 commandline program to compute the AccurateRip checksum of singletrack WAV files.
+ 	 	 	   Implemented according to http://www.hydrogenaudio.org/forums/index.php?showtopic=97603
+ ============================================================================
+ */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sndfile.h>
+
+
+bool check_fileformat(const SF_INFO* sfinfo) {
+#ifdef DEBUG
+	printf("Channels: %i\n", sfinfo->channels);
+	printf("Format: %X\n", sfinfo->format);
+	printf("Frames: %li\n", sfinfo->frames);
+	printf("Samplerate: %i\n", sfinfo->samplerate);
+	printf("Sections: %i\n", sfinfo->sections);
+	printf("Seekable: %i\n", sfinfo->seekable);
+#endif
+
+	if(sfinfo->channels != 2) return false;
+	if((sfinfo->format & SF_FORMAT_TYPEMASK & SF_FORMAT_WAV) != SF_FORMAT_WAV) return false;
+	if((sfinfo->format & SF_FORMAT_SUBMASK & SF_FORMAT_PCM_16) != SF_FORMAT_PCM_16) return false;
+	//if((sfinfo->format & SF_FORMAT_ENDMASK & SF_ENDIAN_LITTLE) != SF_ENDIAN_LITTLE) return false;
+	if(sfinfo->samplerate != 44100) return false;
+
+	return true;
+}
+
+size_t get_full_audiodata_size(const SF_INFO* sfinfo) {
+	// 16bit = samplesize, 8 bit = bitcount in byte
+	return sfinfo->frames * sfinfo->channels * (16 / 8);
+}
+
+u_int32_t* load_full_audiodata(SNDFILE* sndfile, const SF_INFO* sfinfo) {
+	u_int32_t* data = (u_int32_t*)malloc(get_full_audiodata_size(sfinfo));
+
+	if(data == NULL)
+		return NULL;
+
+	if(sf_readf_short(sndfile, (short*)data, sfinfo->frames) !=  sfinfo->frames) {
+		free(data);
+		return NULL;
+	}
+
+	return data;
+}
+
+u_int32_t compute_v1_checksum(const u_int32_t* audio_data, const size_t audio_data_size, const int track_number, const int total_tracks) {
+#define DWORD u_int32_t
+#define SectorBytes 2352
+
+	const DWORD *pAudioData = audio_data;	// this should point entire track audio data
+	int DataSize = 	audio_data_size;	// size of the data
+	int TrackNumber = TrackNumber;	// actual track number on disc, note that for the first & last track the first and last 5 sectors are skipped
+	int AudioTrackCount = total_tracks;	// CD track count
+
+	//---------AccurateRip CRC checks------------
+	DWORD AR_CRC = 0, AR_CRCPosMulti = 1;
+	DWORD AR_CRCPosCheckFrom = 0;
+	DWORD AR_CRCPosCheckTo = DataSize / sizeof(DWORD);
+#define SectorBytes 2352		// each sector
+	if (TrackNumber == 1)			// first?
+		AR_CRCPosCheckFrom+= ((SectorBytes * 5) / sizeof(DWORD));
+	if (TrackNumber == AudioTrackCount)		// last?
+		AR_CRCPosCheckTo-=((SectorBytes * 5) / sizeof(DWORD));
+
+
+	int DataDWORDSize = DataSize / sizeof(DWORD);
+	for (int i = 0; i < DataDWORDSize; i++)
+	{
+		if (AR_CRCPosMulti >= AR_CRCPosCheckFrom && AR_CRCPosMulti <= AR_CRCPosCheckTo)
+			AR_CRC+=(AR_CRCPosMulti * pAudioData[i]);
+
+		AR_CRCPosMulti++;
+	}
+
+	return AR_CRC;
+}
+
+u_int32_t compute_v2_checksum(const u_int32_t* audio_data, const size_t audio_data_size, const int track_number, const int total_tracks) {
+#define DWORD u_int32_t
+#define SectorBytes 2352
+#define __int64 long
+
+	const DWORD *pAudioData = audio_data;	// this should point entire track audio data
+	int DataSize = 	audio_data_size;	// size of the data
+	int TrackNumber = TrackNumber;	// actual track number on disc, note that for the first & last track the first and last 5 sectors are skipped
+	int AudioTrackCount = total_tracks;	// CD track count
+
+	//---------AccurateRip CRC checks------------
+	DWORD AR_CRCPosCheckFrom = 0;
+	DWORD AR_CRCPosCheckTo = DataSize / sizeof(DWORD);
+#define SectorBytes 2352		// each sector
+	if (TrackNumber == 1)			// first?
+		AR_CRCPosCheckFrom+= ((SectorBytes * 5) / sizeof(DWORD));
+	if (TrackNumber == AudioTrackCount)		// last?
+		AR_CRCPosCheckTo-=((SectorBytes * 5) / sizeof(DWORD));
+
+	int DataDWORDSize = DataSize / sizeof(DWORD);
+
+    DWORD AC_CRCNEW = 0;
+	DWORD MulBy = 1;
+	for (int i = 0; i < DataDWORDSize; i++)
+	{
+        DWORD Value = pAudioData[i];
+
+        unsigned __int64 CalcCRCNEW = (unsigned __int64)Value * (unsigned __int64)MulBy;
+        DWORD LOCalcCRCNEW = (DWORD)(CalcCRCNEW & (unsigned __int64)0xFFFFFFFF);
+        DWORD HICalcCRCNEW = (DWORD)(CalcCRCNEW / (unsigned __int64)0x100000000);
+        AC_CRCNEW+=HICalcCRCNEW;
+        AC_CRCNEW+=LOCalcCRCNEW;
+
+        MulBy++;
+	}
+
+	return AC_CRCNEW;
+}
+
+void print_syntax() {
+	puts("Syntax: accuraterip-checksum [--version1 / --version2 (default)] filename track_number total_tracks");
+}
+
+int main(int argc, const char** argv) {
+	if(argc != 4 && argc != 5) {
+		print_syntax();
+		return EXIT_FAILURE;
+	}
+
+	int arg_offset;
+	bool use_v1;
+
+	switch(argc) {
+		case 4:
+			arg_offset = 0;
+			use_v1 = false;
+			break;
+		case 5:
+			arg_offset = 1;
+			if(!strcmp(argv[1], "--version1")) {
+				use_v1 = true;
+			} else if(!strcmp(argv[1], "--version2")) {
+				use_v1 = false;
+			} else {
+				print_syntax();
+				return EXIT_FAILURE;
+			}
+			break;
+		default:
+			print_syntax();
+			return EXIT_FAILURE;
+	}
+
+	const char* filename = argv[1 + arg_offset];
+	const char* track_number_string = argv[2 + arg_offset];
+	const char* total_tracks_string = argv[3 + arg_offset];
+
+	const int track_number = atoi(track_number_string);
+	const int total_tracks = atoi(total_tracks_string);
+
+	if(track_number < 1 || track_number > total_tracks) {
+		puts("Invalid track_number!");
+		return EXIT_FAILURE;
+	}
+
+	if(total_tracks < 1 || total_tracks > 99) {
+		puts("Invalid total_tracks!");
+		return EXIT_FAILURE;
+	}
+
+#ifdef DEBUG
+	printf("Reading %s\n", filename);
+#endif
+
+	SF_INFO sfinfo;
+	sfinfo.channels = 0;
+	sfinfo.format = 0;
+	sfinfo.frames = 0;
+	sfinfo.samplerate = 0;
+	sfinfo.sections = 0;
+	sfinfo.seekable = 0;
+
+	SNDFILE* sndfile = sf_open(filename, SFM_READ, &sfinfo);
+
+	if(sndfile == NULL) {
+		fprintf(stderr, "sf_open failed! sf_error==%i", sf_error(NULL));
+		return EXIT_FAILURE;
+	}
+
+	if(!check_fileformat(&sfinfo)) {
+		fprintf(stderr, "check_fileformat failed!\n");
+		sf_close(sndfile);
+		return EXIT_FAILURE;
+	}
+
+	u_int32_t* audio_data = load_full_audiodata(sndfile, &sfinfo);
+	if(audio_data == NULL) {
+		fprintf(stderr, "load_full_audiodata failed!\n");
+		sf_close(sndfile);
+		return EXIT_FAILURE;
+	}
+
+	const int checksum = use_v1 ?
+			compute_v1_checksum(audio_data, get_full_audiodata_size(&sfinfo), track_number, total_tracks)
+			: compute_v2_checksum(audio_data, get_full_audiodata_size(&sfinfo), track_number, total_tracks);
+
+	printf("%08X\n", checksum);
+
+	sf_close(sndfile);
+	free(audio_data);
+
+	return EXIT_SUCCESS;
+}
